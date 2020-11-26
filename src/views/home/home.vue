be51b6892cccdca1132ee66855feb931967c1de9
chenhui <1549120948@qq.com>
2020-11-26 16:13:44 +0800

    home

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,6 +3,19 @@
   <NavBar id='home-nav'> <div slot="center">购物街</div></NavBar> 
     <home-swiper :banners='banners'></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+    </ul>
 </div>
 </template>
 
@@ -10,14 +23,19 @@
 import NavBar from '@/components/commom/navbar/NavBar.vue'
 import HomeSwiper from "@/views/home/childComps/HomeSwiper.vue"
 import RecommendView from '@/views/home/childComps/commendView.vue'
+import FeatureView from '@/views/home/childComps/FeatureView.vue'
+
 import{getHomeMultiData} from '@/network/home.js'
+
 
 export default {
   name:'home',
   components:{
     NavBar,
    HomeSwiper,
-   RecommendView
+   RecommendView,
+   FeatureView,
+    FeatureView
   },
   data(){
     return {
@@ -38,6 +56,14 @@ export default {
 #home-nav{
   background-color: var(--color-tint);
   color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+#home {
+  padding-top: 44px;
 }
 
 </style>
