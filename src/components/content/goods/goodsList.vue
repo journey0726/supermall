6cd29754af3334a1a31ab5f7191e707821bd1a1f
chenhui <1549120948@qq.com>
2020-11-27 14:16:19 +0800

    home

---
+++ src/components/content/goods/goodsList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="goods">
+      <goods-list-item v-for="item in goods" :goods-item='item'></goods-list-item>
+  </div>
+</template>
+
+<script>
+import goodsListItem from './goodsListItem.vue'
+export default {
+name:'goodsList',
+components:{
+    goodsListItem
+},
+props:{
+    goods:{
+        type:Array,
+        default(){
+            return[]
+        }
+    }
+}
+}
+</script>
+
+<style>
+    .goods{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,28 +4,59 @@
     <home-swiper :banners='banners'></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
     <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
+    <goods-list :goods='goods["pop"].list'></goods-list>
     <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+           <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+           <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+           <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      
+      
     </ul>
 </div>
 </template>
 
 <script>
 import NavBar from '@/components/commom/navbar/NavBar.vue'
+import TabControl from '@/components/content/tabControl/TabControl.vue'
+import GoodsList from '@/components/content/goods/goodsList.vue'
+
 import HomeSwiper from "@/views/home/childComps/HomeSwiper.vue"
 import RecommendView from '@/views/home/childComps/commendView.vue'
 import FeatureView from '@/views/home/childComps/FeatureView.vue'
 
-import{getHomeMultiData} from '@/network/home.js'
+import{getHomeMultiData,getGoodsData} from '@/network/home.js'
+
+
 
 
 export default {
@@ -35,19 +66,44 @@ export default {
    HomeSwiper,
    RecommendView,
    FeatureView,
-    FeatureView
+    FeatureView,
+    TabControl,
+    GoodsList
+    
+    
   },
   data(){
     return {
       banners:[],
-      recommends:[]
+      recommends:[],
+      goods:{
+        'pop':{page:0,list:[]},
+        'new':{page:0,list:[]},
+        'sell':{page:0,list:[]},
+      }
     }
   },
   created(){
-    getHomeMultiData().then(res=>{
+    this.getHomeMultiData()
+    this.getGoodsData('pop')
+    this.getGoodsData('new')
+    this.getGoodsData('sell')
+  },
+  methods:{
+    getHomeMultiData(){
+      getHomeMultiData().then(res=>{
       this.banners = res.data.banner.list
       this.recommends = res.data.recommend.list
     })
+    },
+    getGoodsData(type){
+      const page = this.goods[type].page+1
+      getGoodsData(type,page).then(res=>{
+      this.goods[type].list.push(...res.data.list)
+      this.goods[type].page+=1
+      
+    })
+    }
   }
 }
 </script>
@@ -64,6 +120,12 @@ export default {
 }
 #home {
   padding-top: 44px;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
+  background-color: #fff;
+  z-index: 9;
 }
 
 </style>
